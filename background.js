e2ea50fc843322f7e00bbe7ae81b9c033e24cd6d
Rein <rein@catatonic.ai>
2023-06-03 08:11:40 +0300

    save

--- background.js
+++ background.js
@@ -13,31 +13,79 @@ function escapeHtml(html) {
 
 let modelMap = {};
 
-let socket = new WebSocket('ws://127.0.0.1:3020/');
+let socket;
 
-// Connection opened
-socket.addEventListener('open', function (event) {
-    console.log('Connection opened to LLM service');
-});
-
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    console.log('Message from server: ', event.data);
-});
-
-// Listen for close event
-socket.addEventListener('close', function(event) {
-    console.log('LLM service connection closed', event);
-});
-
-// Connection error
-socket.addEventListener('error', function (event) {
-    console.log('Connection error: ', event);
-});
 
 
 
 chrome.runtime.onMessage.addListener(async function(message, sender, sendResponse) {
+
+
+    console.log("Background got " + message.message + " " + message.service + " " + message.model)
+    console.log(message)
+
+    modelMap[message.clientId] = message;
+
+    console.log(modelMap)
+
+    let getSocket = async function() {
+        if (!socket) {
+            socket = new WebSocket('ws://127.0.0.1:3020/');
+
+            let p = new Promise(function(resolve, reject) {
+                // Connection opened
+                socket.addEventListener('open', function (event) {
+                    console.log('Connection opened to LLM service');
+                    resolve(true);
+                });
+            });
+
+            // Listen for messages
+            socket.addEventListener('message', function (event) {
+                console.log('Message from server: ', event);
+            });
+
+            // Listen for close event
+            socket.addEventListener('close', function(event) {
+                console.log('LLM service connection closed', event);
+                socket = null;
+            });
+
+            // Connection error
+            socket.addEventListener('error', function (event) {
+                console.log('Connection error: ', event);
+            });
+
+            return p;
+        } else {
+            let delay = 500;
+            let p = new Promise(function(resolve, reject) {
+
+                let trySocket = function() {
+                    if (socket.readyState === WebSocket.OPEN) {
+                        if (delay > 500) {
+                            console.log('Connected to LLM service');
+                        }
+                        resolve(true);
+                    } else {
+                        // back off and try again
+                        console.log('No LLM service connection');
+                        delay *= 2;
+                        setTimeout(trySocket, delay)
+                    }
+                }
+
+                trySocket();
+
+            });
+
+            return p;
+
+        }
+    }
+
+    await getSocket();
+
 
     if (message.message === "snapshot" ||
         message.message === "announce") {
@@ -62,11 +110,13 @@ chrome.runtime.onMessage.addListener(async function(message, sender, sendRespons
 
         try {
 
+            // send 'message'
             socket.send(JSON.stringify(message));
 
         } catch (error) {
             console.log('Error:', error);
         }
+
 
     } else {
 
